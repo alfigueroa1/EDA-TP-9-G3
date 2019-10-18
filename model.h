f503180a64076a8d9c5f8ef244e50389b2c88701
Olivia De Vincenti <odevincenti@itba.edu.com>
2019-10-18 17:18:34 -0300

    Empiezo model

--- model.h
+++ model.h
@@ -1,6 +1,22 @@
 #pragma once
+#include <iostream>
+
+
+using namespace std;
 
 class model {
 public:
+
+};
+
+class tweet {
+
+public:
+
+
+private:
+	string date;
+	string username;
+	string tweet;
 
 };
